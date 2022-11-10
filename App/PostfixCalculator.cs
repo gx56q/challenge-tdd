8e9334a11db9e2421158c41f76c796694ca4b1c8
n1gtm <Andreo.7000@gmail.com>
2022-11-10 19:14:57 +0500

    add table16-solution

--- App/PostfixCalculator.cs
+++ App/PostfixCalculator.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace App
 {
@@ -8,7 +7,53 @@ namespace App
     {
         public static string Calculate(string postfixExpression)
         {
-            throw new NotImplementedException();
+            switch (postfixExpression)
+            {
+                case "":
+                    return  "0";
+                case null:
+                    throw new FormatException();
+            }
+            var stack = new Stack<LongComplex>();
+            var postfixArray = postfixExpression.Split(' ');
+            foreach (var item in postfixArray)
+            {
+                if (item is not ("+" or "-" or "*" or "/" or "^"))
+                {
+                    var number = LongComplex.Parse(item);
+                    stack.Push(number);
+                }
+                else
+                {
+                    if (stack.Count < 2)
+                        throw new FormatException();
+                    var secondValue = stack.Pop();
+                    var firstValue = stack.Pop();
+                    switch (item)
+                    {
+                        case "+":
+                            stack.Push(LongComplex.Add(firstValue, secondValue));
+                            break;
+                        case "-":
+                            stack.Push(LongComplex.Subtract(firstValue, secondValue));
+                            break;
+                        case "*":
+                            stack.Push(LongComplex.Multiply(firstValue, secondValue));
+                            break;
+                        case "/":
+                            stack.Push(LongComplex.Parse((firstValue.Real / (int)secondValue.Real).ToString()));
+                            break;
+                        case "^":
+                            stack.Push(LongComplex.Parse(((int)Math.Pow(firstValue.Real, secondValue.Real)).ToString()));
+                            break;
+                        default:
+                            throw new FormatException();
+                    }
+                }
+            }
+            if (stack.Count == 2)
+                throw new FormatException();
+            return stack.Pop().ToString();
         }
     }
 }
